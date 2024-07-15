0f4fd9e1fdab07c2f706ad60b9c2f3222916beee
frizchar <cchrysanth@gmail.com>
2024-07-15 15:27:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import functions
 
 # Input parameters: ticker_symbol, start_date, end_date
 ticker_symbol = "AAPL"
+# input dates in 'YYYY-MM-DD' format
 start_date = "2020-01-01"
 end_date = "2023-12-31"
 data = functions.get_yahoo_finance_data(ticker_symbol, start_date, end_date)
