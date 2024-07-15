300cb04679bbe98a91cd627cb9974591e04c08e0
frizchar <cchrysanth@gmail.com>
2024-07-15 15:28:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ print(data.head())
 
 
 if __name__ == '__main__':
-    functions.plot_stock(data)
+    functions.plot_timeseries(data)
     functions.plot_filtered_data(data)
     functions.plot_3d_phase_space(data)
     functions.plot_3d_phase_space_of_filtered_data(data)
