35b21240b50717b0186b773939c45305980562fb
frizchar <cchrysanth@gmail.com>
2024-07-16 11:48:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ end_date = "2023-12-31"     # End date of historical data in 'YYYY-MM-DD' format
 cutoff_freq = 0.01          # Cutoff frequency for the low-pass filter
 sample_rate = 1             # Sampling rate of the input data
 order = 4                   # Order of the Butterworth filter
+time_delay_parameter = 5    # Time delay parameter of phase space embedding
 ###########################
 
 
@@ -20,5 +21,5 @@ if __name__ == '__main__':
 
     functions.plot_timeseries(data)
     functions.plot_filtered_timeseries(data, filtered_close_prices)
-    functions.plot_3d_phase_space(close_prices)
-    functions.plot_3d_phase_space_of_filtered_data(filtered_close_prices)
+    functions.plot_3d_phase_space(close_prices, time_delay_parameter)
+    functions.plot_3d_phase_space_of_filtered_data(filtered_close_prices, time_delay_parameter)
